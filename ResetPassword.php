c8d9c9a0786057bae904f7c845c1da45ea70fd89
ahmadr221b1 <ahmadr221b@gmail.com>
2023-03-21 15:23:59 -0400

    Update ResetPassword.php
    reset password requirements added

--- ResetPassword.php
+++ ResetPassword.php
@@ -44,14 +44,80 @@
       nomodule
       src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"
     ></script>
+    <script>
+      function validatePassword() {
+        const password = document.getElementsByName("pass")[0];
+        const confirmPassword = document.getElementsByName("confirmPass")[0];
+        const passwordRequirements =
+          /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,}$/;
+
+        if (
+          !passwordRequirements.test(password.value) ||
+          !passwordRequirements.test(confirmPassword.value)
+        ) {
+          alert(
+            "Password and Confirm Password must be at least 8 characters long, contain one upper case letter, one lower case letter, one number, and one special character."
+          );
+          return false;
+        }
+
+        if (password.value !== confirmPassword.value) {
+          alert("Password and Confirm Password do not match.");
+          return false;
+        }
+
+        return true;
+      }
+
+      function validateForm() {
+        return validatePassword();
+      }
+
+      function updateRequirements() {
+        const password = document.getElementsByName("pass")[0].value;
+        const requirements = {
+          length: /(?=.{8,})/,
+          lowerCase: /(?=.*[a-z])/,
+          upperCase: /(?=.*[A-Z])/,
+          number: /(?=.*\d)/,
+          specialChar: /(?=.*[!@#$%^&*])/,
+        };
+
+        for (const key in requirements) {
+          const reqElement = document.getElementById(key);
+          if (requirements[key].test(password)) {
+            reqElement.classList.add("matched");
+            reqElement.classList.remove("not-matched");
+          } else {
+            reqElement.classList.add("not-matched");
+            reqElement.classList.remove("matched");
+          }
+        }
+      }
+    </script>
   </head>
   <body class="ResetPasswordPage">
     <div class="Left"><img src="./Logo.png" alt="Logo" /></div>
-    <form class="sign-up-form ResetPassword" action="ResetPassword.php" name="ResetPassword" method="POST">
+    <form class="sign-up-form ResetPassword" action="ResetPassword.php" name="ResetPassword" method="POST" onsubmit="return validateForm()">
       <img src="./Brand-Logo.png" alt="Brand Logo" />
       <h1>Reset Password</h1>
-      <input type="password" name="pass" placeholder="New Password" />
-      <input type="password" placeholder="Confirm Password" />
+      <input type="password" name="pass" placeholder="New Password" oninput="updateRequirements()" required/>
+      <input type="password" placeholder="Confirm Password" required/>
+      <ul style="width: 218.64px" class="password-requirements">
+        <li id="length" class="not-matched">8 characters minimum</li>
+        <li id="upperCase" class="not-matched">One uppercase letter</li>
+        <li id="lowerCase" class="not-matched">One lowercase letter</li>
+      </ul>
+
+      <ul class="password-requirements">
+        <li id="number" class="not-matched">One number</li>
+        <li id="specialChar" class="not-matched">
+          One special character (!@#$%^&*)
+        </li>
+        <li id="confirm-password" class="not-matched">
+          Confirm password match password
+        </li>
+      </ul>
       <input type="submit" id="btn" value="Reset" name="submitFromResetPassword" />
       <input type="hidden" name="secretValue1" value="<?PHP $email = $_GET['email']; echo $email; ?>">
       <input type="hidden" name="secretValue2" value="<?PHP $code = $_GET['code']; echo $code; ?>">
